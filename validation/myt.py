67901873a314a597638fbaf37cebec8d52dd3781
wenknow <wc9241@gmail.com>
2025-03-03 20:07:37 +0800

    fix

--- validation/myt.py
+++ validation/myt.py
@@ -3,6 +3,8 @@ import os
 import shutil
 
 import bittensor as bt
+from pydantic import BaseModel
+
 from neuralai.miner.utils import generate
 from neuralai.protocol import NATextSynapse
 from validation.models import ValidateRequest
@@ -14,11 +16,11 @@ async def test_my_score():
     prompt_text = "A stainless steel chef's knife with a comfortable ergonomic handle and a razor-sharp blade."
     destination_folder = './validation/results'
 
-    synapse = bt.Synapse()
-    synapse.prompt_text = prompt_text
-    synapse.timeout = 120
+    synapse2 = bt.Synapse()
+    synapse2.prompt_text = prompt_text
+    synapse2.timeout = 120
     # 生成模型
-    synapse = await generate(synapse)
+    synapse = await generate(synapse2)
     print(f"success generate synapse:{synapse}")
 
     # 复制文件到验证器文件夹
