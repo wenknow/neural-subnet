4b609b33890dd0dc6794d7d33f59350b7b90973c
wenknow <wc9241@gmail.com>
2025-03-03 17:37:34 +0800

    add

---
+++ validation/myt.py
@@ -0,0 +1,36 @@
+import os
+import shutil
+
+from neuralai.miner.utils import generate
+from neuralai.protocol import NATextSynapse
+from validation.models import ValidateRequest
+from validation.validation_endpoint import Validation
+
+
+async def test_my_score():
+    validation = Validation()
+    prompt_text = "A stainless steel chef's knife with a comfortable ergonomic handle and a razor-sharp blade."
+    destination_folder = './validation/results'
+
+    synapse = NATextSynapse(prompt_text=prompt_text)
+    # 生成模型
+    synapse = await generate(synapse)
+    print(f"success generate synapse:{synapse}")
+
+    # 复制文件到验证器文件夹
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder)
+    try:
+        shutil.copy("../generate/output/preview.png", destination_folder)
+        shutil.copy("../generate/output/output.glb", destination_folder)
+        print(f"文件已成功复制到 {destination_folder}")
+    except Exception as e:
+        print(f"复制文件时出错: {e}")
+
+    resp = ValidateRequest(prompt=prompt_text)
+    # 校验得分
+    score = validation.validate(resp)
+    print(f"test_my_score: {score}")
+
+if __name__ == '__main__':
+    test_my_score()
