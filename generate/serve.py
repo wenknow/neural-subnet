add4834c922398c1cb5427647f81cd6f923972cc
wenknow <wc9241@gmail.com>
2025-03-05 20:46:07 +0800

    fix

--- generate/serve.py
+++ generate/serve.py
@@ -79,7 +79,7 @@ async def text_to_3d(prompt: str = Body()):
 
     # Stage 1: Text to Image
     start = time.time()
-    # prompt = unquote_plus(prompt)
+    prompt = unquote_plus(prompt)
     print(f"get prompt: {prompt}")
     res_rgb_pil = text_to_image_model(
         prompt,
