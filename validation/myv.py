2545e2cc9e3ffb45a14ed811d927eeecc0c26b0d
wenknow <wc9241@gmail.com>
2025-03-07 12:33:42 +0800

    fix

--- validation/myv.py
+++ validation/myv.py
@@ -5,11 +5,11 @@ from validation.models import ValidateRequest
 from validation.validation_endpoint import Validation
 
 
-async def test_my_score(prompt):
+async def test_my_score(uid, prompt):
     prompt_text = prompt
 
     validation = Validation()
-    resp = ValidateRequest(prompt=prompt_text, uid=173)
+    resp = ValidateRequest(prompt=prompt_text, uid=uid)
     # 校验得分
     score = validation.validate(resp)
     print(f"test_my_score: {score}")
@@ -17,7 +17,9 @@ async def test_my_score(prompt):
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        prompt = sys.argv[1]
+        uid = sys.argv[1]
+        prompt = sys.argv[2]
     else:
+        uid = 173
         prompt = "A stainless steel chef's knife with a comfortable ergonomic handle and a razor-sharp blade."
-    asyncio.run(test_my_score(prompt))
+    asyncio.run(test_my_score(uid, prompt))
